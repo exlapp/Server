0b1329487eb31ce27d578c903944ed3224936c65
exlapp <alvest@bk.ru>
2025-04-27 18:55:27 +0400

    feat <authRouter.ts>: added a new post request '/forgotPassword'
    these changes were made to implement the user password reset functionality
    
    Signed-of-by: Alexandr Burenin Issue: BACKEND-1

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getAuth, createUser, newPasswordSet } from '../controllers/authController';
+import { getAuth, createUser, newPasswordSet, forgotPassword } from '../controllers/authController';
 import { executeAuthChain } from '../helpers/executeAuthChain'
 import { executeRegChain } from '../helpers/executeRegChain'
 import { executeResetChain } from '../helpers/executeResetChain'
@@ -9,6 +9,7 @@ const router: Router = Router();
 router
     .post('/signIn', executeAuthChain(), getAuth)
     .post('/signUp', executeRegChain(), createUser)
-    .post('/newPasswordSet', executeResetChain(), newPasswordSet);
+    .post('/newPasswordSet', executeResetChain(), newPasswordSet)
+    .post('/forgotPassword', forgotPassword)
 
 export { router as authRouter };
