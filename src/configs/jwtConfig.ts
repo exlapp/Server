12e83b5149d6e9e91e5a0a17b536640ac5a16bde
exlapp <alvest@bk.ru>
2025-04-27 18:37:16 +0400

    chore <jwtConfig.ts>: добавлены jwtRestoreSecretKey,  jwtRestoreExpiresIn
    These changes were made to enable the formation of a restoreToken as part of the user's password reset functionality.
    
    Signed-of-by: Alexandr Burenin Issue: BACKEND-1

--- src/configs/jwtConfig.ts
+++ src/configs/jwtConfig.ts
@@ -4,17 +4,21 @@ import { config } from 'dotenv'
 interface IConfig {    
     jwtAccessSecretKey: string;
     jwtRefreshSecretKey: string;
+    jwtRestoreSecretKey: string;
     jwtAccessExpiresIn: number;
-    jwtRefreshExpiresIn: number    
+    jwtRefreshExpiresIn: number;
+    jwtRestoreExpiresIn: number;
 }
 
 config().parsed;
 
-const { jwtAccessExpiresIn, jwtAccessSecretKey, jwtRefreshExpiresIn, jwtRefreshSecretKey }: IConfig = {
+const { jwtAccessExpiresIn, jwtAccessSecretKey, jwtRestoreSecretKey, jwtRefreshExpiresIn, jwtRefreshSecretKey, jwtRestoreExpiresIn }: IConfig = {
     jwtAccessSecretKey: String(env.JWT_ACCESS_SECRET_KEY),
     jwtAccessExpiresIn: Number(env.JWT_ACCESS_EXPIRES_IN),
     jwtRefreshSecretKey: String(env.JWT_REFRESH_SECRET_KEY),
-    jwtRefreshExpiresIn: Number(env.JWT_REFRESH_EXPIRES_IN),   
+    jwtRefreshExpiresIn: Number(env.JWT_REFRESH_EXPIRES_IN),
+    jwtRestoreSecretKey: String(env.JWT_RESTORE_SECRET_KEY),
+    jwtRestoreExpiresIn: Number(env.JWT_RESTORE_EXPIRES_IN),    
 };
 
-export { jwtAccessExpiresIn, jwtAccessSecretKey, jwtRefreshExpiresIn, jwtRefreshSecretKey };
+export { jwtAccessExpiresIn, jwtAccessSecretKey, jwtRefreshExpiresIn, jwtRefreshSecretKey, jwtRestoreSecretKey, jwtRestoreExpiresIn };
